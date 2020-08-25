295f2e2d4dd4ac770379d6873dac617c9ca99a7e
Asus <Asus@DESKTOP-QL04J0F>
2020-08-26 00:24:25 +0600

    Create final class using final variable

---
+++ Final Variable/src/FinalVariable.java
@@ -0,0 +1,12 @@
+
+public class FinalVariable {
+	final int speedlimit=100; 
+	public void run(){  
+	  System.out.println("Speed Limit: " +speedlimit);  
+	 }  
+	 public static void main(String args[]){  
+	 FinalVariable obj=new  FinalVariable();  
+	 obj.run();  
+	 }  
+	
+}
